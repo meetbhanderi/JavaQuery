638ef78966b7f47bcc66da43bf3a9286e77fc855
Bhanderi Meet <55754435+meetbhanderi@users.noreply.github.com>
2020-10-11 11:48:25 +0530

    Update AppleAndOrangeMain.java
    used foreach loop instead of for loop, because its easy and faster than forloop and overall consist better performance and also added that user input the points so, user do not hesitating to change the code and check, here i used user inputs so, user every time run the code and give inputs as user's choice.

--- AppleAndOrangeMain.java
+++ AppleAndOrangeMain.java
@@ -1,26 +1,74 @@
-class AppleAndOrange{
-    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
-        int appleCount=0,orangeCount=0;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-        for(int i=0; i<apples.length;i++){
-            if(a+apples[i]>=s && a+apples[i]<=t){
+public class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+	int appleCount=0,orangeCount=0;
+
+        for(int i : apples){
+            if(a+i>=s && a+i<=t){
                 appleCount++;
             }
         }
-        for(int o=0;o<oranges.length;o++){
-            if(b+oranges[o]>=s && b+oranges[o]<=t){
+        for(int o : oranges){
+            if(b+o>=s && b+o<=t){
                 orangeCount++;
             }
         }
         System.out.println(appleCount);
         System.out.println(orangeCount);
     }
-}
 
-class AppleAndOrangeMain{
-    public static void main(String[] args){
-    	    int[] apples = {2,3,-4};
-	    int[] oranges = {3,-2,-4};
-           AppleAndOrange.countApplesAndOranges(7,10,4,12,apples,oranges);
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
     }
 }
