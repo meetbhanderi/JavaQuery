b8d4dff7223514cf50802fb94304ad30f90c522e
Trushit777 <52493492+Trushit777@users.noreply.github.com>
2020-10-12 04:21:19 +0000

    Update AppleAndOrangeMain.java
    Unnecessary Import statements removed and added necessary import statement for reducing the time complexity.
    Also done documentation and change logs to the code.

--- AppleAndOrangeMain.java
+++ AppleAndOrangeMain.java
@@ -1,74 +1,80 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
+/**
+ * @author @Trushit777
+ * @change Unnecessary Imports removed and necessary import added to the code.
+ * @since 12-10-2020
+ */
 public class Solution {
 
-    // Complete the countApplesAndOranges function below.
-    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
-	int appleCount=0,orangeCount=0;
+	/**
+	 * @param s
+	 * @param t
+	 * @param a
+	 * @param b
+	 * @param apples
+	 * @param oranges
+	 */
+	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+		int appleCount=0,orangeCount=0;
 
-        for(int i : apples){
-            if(a+i>=s && a+i<=t){
-                appleCount++;
-            }
-        }
-        for(int o : oranges){
-            if(b+o>=s && b+o<=t){
-                orangeCount++;
-            }
-        }
-        System.out.println(appleCount);
-        System.out.println(orangeCount);
-    }
+		for(int i : apples){
+			if(a+i>=s && a+i<=t){
+				appleCount++;
+			}
+		}
+		for(int o : oranges){
+			if(b+o>=s && b+o<=t){
+				orangeCount++;
+			}
+		}
+		System.out.println(appleCount);
+		System.out.println(orangeCount);
+	}
 
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        String[] st = scanner.nextLine().split(" ");
+	public static void main(String[] args) {
+		String[] st = scanner.nextLine().split(" ");
 
-        int s = Integer.parseInt(st[0]);
+		int s = Integer.parseInt(st[0]);
 
-        int t = Integer.parseInt(st[1]);
+		int t = Integer.parseInt(st[1]);
 
-        String[] ab = scanner.nextLine().split(" ");
+		String[] ab = scanner.nextLine().split(" ");
 
-        int a = Integer.parseInt(ab[0]);
+		int a = Integer.parseInt(ab[0]);
 
-        int b = Integer.parseInt(ab[1]);
+		int b = Integer.parseInt(ab[1]);
 
-        String[] mn = scanner.nextLine().split(" ");
+		String[] mn = scanner.nextLine().split(" ");
 
-        int m = Integer.parseInt(mn[0]);
+		int m = Integer.parseInt(mn[0]);
 
-        int n = Integer.parseInt(mn[1]);
+		int n = Integer.parseInt(mn[1]);
 
-        int[] apples = new int[m];
+		int[] apples = new int[m];
 
-        String[] applesItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String[] applesItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < m; i++) {
-            int applesItem = Integer.parseInt(applesItems[i]);
-            apples[i] = applesItem;
-        }
+		for (int i = 0; i < m; i++) {
+			int applesItem = Integer.parseInt(applesItems[i]);
+			apples[i] = applesItem;
+		}
 
-        int[] oranges = new int[n];
+		int[] oranges = new int[n];
 
-        String[] orangesItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String[] orangesItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < n; i++) {
-            int orangesItem = Integer.parseInt(orangesItems[i]);
-            oranges[i] = orangesItem;
-        }
+		for (int i = 0; i < n; i++) {
+			int orangesItem = Integer.parseInt(orangesItems[i]);
+			oranges[i] = orangesItem;
+		}
 
-        countApplesAndOranges(s, t, a, b, apples, oranges);
+		countApplesAndOranges(s, t, a, b, apples, oranges);
 
-        scanner.close();
-    }
+		scanner.close();
+	}
 }
